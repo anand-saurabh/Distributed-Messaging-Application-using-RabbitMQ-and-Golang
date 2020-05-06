b37fd20ca82f03dc1ddd3f6cbe47cad06f6a176a
anand-saurabh <saurabh.anand@ufl.edu>
2020-05-06 18:28:51 -0400

    running code to publish messages to RabbitMQ queue

---
+++ distributed/dto/dto.go
@@ -0,0 +1,20 @@
+package dto
+
+
+import (
+  "encoding/gob"
+  "time"
+)
+
+
+type SensorMessage struct {
+  Name string
+  Value float64
+  Timestamp time.Time
+
+
+}
+
+func init()  {
+  gob.Register(SensorMessage{})
+}
--- distributed/sensors/sensor.go
+++ distributed/sensors/sensor.go
@@ -1,6 +1,6 @@
 package main
 
-// this generates the sensor data to be pushed to rabbitmq 
+// this generates the sensor data to be pushed to rabbitmq.
 import
 (
   "flag"
@@ -8,8 +8,14 @@ import
   "strconv"
   "math/rand"
   "log"
+  "encoding/gob"
+  "distributed/dto"
+  "bytes"
+  "distributed/qutils"
+  "github.com/streadway/amqp"
 )
 
+var url = "amqp://guest:guest@localhost:5672"
 var name = flag.String("name", "sensor", "name of the sensor")
 var freq = flag.Uint("freq", 5, "update the frequency")
 var max = flag.Float64("max", 5., "max val for generated readings")
@@ -20,16 +26,42 @@ var value = r.Float64()*(*max - * min) + * min
 var nom = (*max  - * min)/2 + *min
 var r = rand.New(rand.NewSource(time.Now().UnixNano()))
 
+
 func main()  {
 
   flag.Parse()
 
+  conn, ch := qutils.GetChannel(url)
+  defer conn.Close()
+  defer ch.Close()
+  dataQueue := qutils.GetQueue(*name, ch)
   dur, _ := time.ParseDuration(strconv.Itoa(1000/(int)(*freq)) + "ms")
 
   signal := time.Tick(dur)
 
+  buf := new(bytes.Buffer)
+  enc := gob.NewEncoder(buf)
   for range signal {
     calcVal()
+    reading := dto.SensorMessage{
+      Name: *name,
+      Value : value,
+      Timestamp : time.Now(),
+    }
+
+    buf.Reset()
+    enc.Encode(reading)
+
+    msg := amqp.Publishing {
+      Body : buf.Bytes(),
+    }
+
+    ch.Publish(
+      "",
+      dataQueue.Name,
+      false,
+      false,
+      msg)
     log.Printf("reading sent. value %v \n", value)
   }
 }
